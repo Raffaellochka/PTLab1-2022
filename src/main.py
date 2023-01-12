b012dd5729a0ae5269821ec98ea166c8e81d86ff
Raffaellochka <122486410+Raffaellochka@users.noreply.github.com>
2023-01-12 11:15:02 +0300

    Исправил код

--- src/main.py
+++ src/main.py
@@ -19,11 +19,11 @@ def get_path_from_arguments(args) -> str:
 def main():
     path = get_path_from_arguments(sys.argv[1:])
 
-    reader = TextDataReader()
+    reader = TextDataReaderYAML()
     students = reader.read(path)
     print("Students: ", students)
 
-    rating = CalcRating(students).calc()
+    rating = TopStudent(students).calc()
     print("Rating: ", rating)
 
 
